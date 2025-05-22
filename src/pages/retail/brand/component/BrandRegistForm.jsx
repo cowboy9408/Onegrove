fc192875d2f0d6f6e997a8e5c67d5d9dca2861c0
byungkil.Park <byungkil.park@emotion.co.kr>
2025-05-22 09:29:21 +0900

    충돌 수정

--- src/pages/retail/brand/component/BrandRegistForm.jsx
+++ src/pages/retail/brand/component/BrandRegistForm.jsx
@@ -19,10 +19,6 @@ const BrandRegistForm = forwardRef(({ lang, readOnly = false }, ref) => {
   });
   const { control, register, setValue, watch, getValues } = methods;
 
-<<<<<<< HEAD
-  
-=======
->>>>>>> origin/byeongjun
   const editorRef = useRef();
   const { showModal } = useModal();
 
@@ -36,11 +32,7 @@ const BrandRegistForm = forwardRef(({ lang, readOnly = false }, ref) => {
         console.error("카테고리 목록 불러오기 실패:", err);
       }
     };
-<<<<<<< HEAD
-    fetchCategory(); 
-=======
     fetchCategory();
->>>>>>> origin/byeongjun
 
     const fetchKeyword = async () => {
       try {
@@ -51,12 +43,7 @@ const BrandRegistForm = forwardRef(({ lang, readOnly = false }, ref) => {
         console.error("키워드 목록 불러오기 실패:", err);
       }
     };
-<<<<<<< HEAD
-    // fetchKeyword(); 
-    
-=======
     // fetchKeyword();
->>>>>>> origin/byeongjun
   }, []);
 
   const validateRequiredFields = () => {
@@ -182,43 +169,6 @@ const BrandRegistForm = forwardRef(({ lang, readOnly = false }, ref) => {
 
               const options = [
                 {
-<<<<<<< HEAD
-                    "code": "key0101",
-                    "value": "Man"
-                },
-                {
-                    "code": "key0102",
-                    "value": "Woman"
-                },
-                {
-                    "code": "key0103",
-                    "value": "Lifewear"
-                },
-                {
-                    "code": "key0104",
-                    "value": "Street Fashion"
-                },
-                {
-                    "code": "key0105",
-                    "value": "Sportswear"
-                },
-                {
-                    "code": "key0106",
-                    "value": "SPA"
-                },
-                {
-                    "code": "key0107",
-                    "value": "Luxury"
-                },
-                {
-                    "code": "key0108",
-                    "value": "Kids"
-                },
-                {
-                    "code": "key0109",
-                    "value": "Beauty"
-                }
-=======
                   code: "key0101",
                   value: "Man",
                 },
@@ -254,7 +204,6 @@ const BrandRegistForm = forwardRef(({ lang, readOnly = false }, ref) => {
                   code: "key0109",
                   value: "Beauty",
                 },
->>>>>>> origin/byeongjun
               ];
 
               return (
