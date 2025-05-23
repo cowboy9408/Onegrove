a421ab236a90fbba74eaa71e7d1568b992fb79cf
byungkil.Park <byungkil.park@emotion.co.kr>
2025-05-23 11:07:27 +0900

    이미지 object test

--- src/pages/retail/brand/component/BrandRegistForm.jsx
+++ src/pages/retail/brand/component/BrandRegistForm.jsx
@@ -71,16 +71,17 @@ const BrandRegistForm = forwardRef(({ lang, readOnly = false }, ref) => {
         if (!file || !file.name) return null;
 
         return {
-          id: null,
           originalName: file.originalName || file.name,
           name: file.name,
           size: file.size,
-          extension: "." + file.originalName.split(".").pop(),
+          extension: "." + (file.originalName || file.name).split(".").pop(),
           mime: file.type || "image/png",
-          classification: null,
-          path: `C:\\\\upload\\/test\\${file.name}`,
+          classification: file.classification ?? "brand",
+          path: file.path,
+          // status: file.status ?? "R",
           status: null,
         };
+          
       };
 
       return {
@@ -316,18 +317,6 @@ const BrandRegistForm = forwardRef(({ lang, readOnly = false }, ref) => {
         <Upload
           name="contentImage5"
           label="본문 이미지 5"
-          required
-          readOnly={readOnly}
-        />
-        <Upload
-          name="pcBodyImage"
-          label="PC 본문 이미지"
-          required
-          readOnly={readOnly}
-        />
-        <Upload
-          name="moBodyImage"
-          label="MO 본문 이미지"
           required
           readOnly={readOnly}
         />
