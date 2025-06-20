2d81503578316529b1d1e3dd52cec74f0e16a14e
byeongjun.kim <byeongjun.kim@emotion.co.kr>
2025-06-20 22:18:09 +0900

    대시보드 수정

--- src/pages/dashboard/DashboardPage.jsx
+++ src/pages/dashboard/DashboardPage.jsx
@@ -1,6 +1,6 @@
 export default function DashboardPage() {
   return (
-    <div className="min-h-[830px]">
+    <div className="min-h-[830px] text-center">
       원그로브 관리자 전용 홈페이지에 오신걸 환영합니다!
     </div>
   );
