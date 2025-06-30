7dc8b93ff7378d2ede7acfb6d21cd3e688e215e9
byungkil.Park <byungkil.park@emotion.co.kr>
2025-06-30 16:51:07 +0900

    admin lnb 권한 수정

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -717,7 +717,7 @@ export const routeMeta = [
         title: "오피스 관리",
         icon: <CalendarDaysIcon size={18} />,
         hidden: false,
-        permissions: ["SUPER_ADMIN", "NORMAL_ADMIN", "RETAIL_ADMIN", "MEMBER"],
+        permissions: ["SUPER_ADMIN", "NORMAL_ADMIN", "OFFICE_ADMIN", "OFFICE_SECRETARY_ADMIN"],
         children: [
           {
             uuid: "b3c13f6a-0715-4199-b0bd-dcc163d4c89d",
@@ -728,10 +728,8 @@ export const routeMeta = [
             permissions: [
               "SUPER_ADMIN",
               "NORMAL_ADMIN",
-              "RETAIL_ADMIN",
               "OFFICE_ADMIN",
               "OFFICE_SECRETARY_ADMIN",
-              "MEMBER",
             ],
           },
           {
@@ -743,10 +741,8 @@ export const routeMeta = [
             permissions: [
               "SUPER_ADMIN",
               "NORMAL_ADMIN",
-              "RETAIL_ADMIN",
               "OFFICE_ADMIN",
               "OFFICE_SECRETARY_ADMIN",
-              "MEMBER",
             ],
           },
           {
@@ -758,10 +754,8 @@ export const routeMeta = [
             permissions: [
               "SUPER_ADMIN",
               "NORMAL_ADMIN",
-              "RETAIL_ADMIN",
               "OFFICE_ADMIN",
-              "OFFICE_SECRETARY_ADMIN",
-              "MEMBER",
+              "OFFICE_SECRETARY_ADMIN"
             ],
           },
           {
@@ -773,9 +767,7 @@ export const routeMeta = [
             permissions: [
               "SUPER_ADMIN",
               "NORMAL_ADMIN",
-              "RETAIL_ADMIN",
               "OFFICE_ADMIN",
-              "MEMBER",
             ],
           },
         ],
