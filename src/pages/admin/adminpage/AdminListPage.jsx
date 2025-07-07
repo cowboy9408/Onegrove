99e022d108bd801392d70efa0b473c408ff31e8c
byeongjun.kim <byeongjun.kim@emotion.co.kr>
2025-07-07 16:43:11 +0900

    유저 리스트 시간 수정

--- src/pages/admin/adminpage/AdminListPage.jsx
+++ src/pages/admin/adminpage/AdminListPage.jsx
@@ -131,9 +131,9 @@ export default function AdminListPage() {
                     year: "numeric",
                     month: "2-digit",
                     day: "2-digit",
-                    hour: "2-digit",
-                    minute: "2-digit",
-                    second: "2-digit",
+                    // hour: "2-digit",
+                    // minute: "2-digit",
+                    // second: "2-digit",
                   })
                 : "",
             }))
