3e50c3f878a76c9b5db78d920189ebf3927473f6
byungkil.Park <byungkil.park@emotion.co.kr>
2025-07-22 14:52:13 +0900

    빌드콘솔삭제

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import strip from '@rollup/plugin-strip';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    strip({
+      include: '**/*.(js|ts|jsx|tsx)', // 제거 대상
+      functions: ['console.*', 'assert.*', 'debug', 'alert'], // 제거할 함수들
+    }),
+  ],
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
